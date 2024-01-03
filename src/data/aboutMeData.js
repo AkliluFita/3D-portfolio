2ebd0ee7f88698ec0b565e12cb8010a14cfaaee2
AkliluFita <weaba123abdu@gmail.com>
2024-01-03 12:21:17 +0300

    about me section updated

--- src/data/aboutMeData.js
+++ src/data/aboutMeData.js
@@ -2,6 +2,7 @@ import frontend from "../assets/about/frontend-100.png";
 import backend from "../assets/about/backend-100.png";
 import fullstack from "../assets/about/full-stack-100.png";
 import machineLearning from "../assets/about/machine-learning-100.png";
+import uxDesign from "../assets/about/ux-design.png";
 
 export const aboutMeProfileData = ` As a Master Computer Engineer, I have gained extensive knowledge and experience in various computer-related courses. 
 With a strong passion for web design and development, I have acquired proficiency in diverse technologies, enabling me to pursue my goal of becoming a skilled and well experienced web developer. 
@@ -61,6 +62,17 @@ export const aboutMeCardData = [
     desc: `I specialize in text classification. Using advanced NLP techniques, 
     I develop models that accurately categorize and analyze textual data,
      enabling businesses to extract valuable insights and automate content classification.`,
+    experiencePeriod: 1,
+  },
+  {
+    id: 1,
+    icon: uxDesign,
+    title: "UI/UX Designer",
+    tool: ["Figma", "Canva", "Photo shope"],
+    desc: `I am an experienced UI/UX designer specializing in Figma, with additional proficiency in Canva and Photoshop. 
+    My expertise lies in creating user-friendly interfaces, and I have a keen eye for detail. With Figma as my primary tool,
+     I craft visually appealing designs that prioritize user experience.
+     `,
     experiencePeriod: 1,
   },
 ];
